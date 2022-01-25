eaa16a9b74dea392297b59ea35999fb01f90e5e6
GeorgeMacPherson <90939183+GeorgeMacPherson@users.noreply.github.com>
2022-01-26 12:32:46 +1300

    Database Started

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -4,7 +4,11 @@
  * required.
  */
 
+drop table if exists users;
+drop table if exists articles;
+drop table if exists comments;
 drop table if exists test;
+
 
 create table test (
     id integer not null primary key,
@@ -14,3 +18,37 @@ create table test (
 insert into test (stuff) values
     ('Things'),
     ('More things');
+
+create table users(
+id integer not null primary key,
+fname varchar(32),
+lname varchar(32),
+bio varchar(500),
+username varchar(32),
+password varchar(150),
+dob date,
+avatar varchar(150)
+);
+
+create table articles(
+id integer not null primary key,
+title varchar(32),
+image varchar(150),
+ingredients varchar(2000),
+method varchar(5000),
+creator_user_id integer,
+foreign key (creator_user_id) references users (id)
+);
+
+create table comments(
+id integer not null,
+article_id integer not null,
+time_created timestamp,
+parent_comment_id integer default 0,
+level integer default 0,
+content varchar(500),
+user_id integer not null,
+primary key (id, article_id),
+foreign key (user_id) references users (id),
+foreign key (article_id) references articles (id)
+);
