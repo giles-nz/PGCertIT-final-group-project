b442f8eb8262ada27df68d92bf341b5318213de7
GeorgeMacPherson <90939183+GeorgeMacPherson@users.noreply.github.com>
2022-02-03 11:22:18 +1300

    username nearly working

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ const fs = require("fs");
 // Setup JIMP for image processing
 const jimp = require("jimp");
 
+//Accessing database
+const SQL = require("sql-template-strings");;
+const dbPromise = app.use(require("./modules/database.js"));
+
 // Use the toaster middleware
 app.use(require("./middleware/toaster-middleware.js"));
 
