3af7dc32e44c44a2e6f8045eebf6ed2a248a5d50
giles-nz <gbro377@aucklanduni.ac.nz>
2022-02-07 15:54:10 +1300

    Giles changes

--- public/script/script.js
+++ public/script/script.js
@@ -43,19 +43,15 @@ window.addEventListener("load", function(){
     });
     
     //matching child comment submit button...
-     const submit_second = document.querySelectorAll(".submit-second");
+    const submit_second = document.querySelectorAll(".submit-second");
 
-     //when people click the submit button, this function will call...
-     submit_second.forEach(function(ele){
-         ele.addEventListener("click", commentreFresh);
-     })
+    //when people click the submit button, this function will call...
+    submit_second.forEach(function(ele){
+        ele.addEventListener("click", commentreFresh);
+    });
 
-
-
-
-
-     // this function can be called, using AJAX/Fetch to retrieve all comments and display...
-     async function commentreFresh(){
+    // this function can be called, using AJAX/Fetch to retrieve all comments and display...
+    async function commentreFresh(){
         let request = await fetch(`http://localhost:3000/commentUpdate`);
         let allComments = await request.json();
         console.log(allComments.length);
@@ -67,21 +63,23 @@ window.addEventListener("load", function(){
             //using loop to write all comment...
             commentDisplay.innerHTML += `
             <div class="comment-card">
-             <div class="comment-avater"><img class="comment-image" src="./images/avatars/${allComments[i].avatar}.png"></div>
-              <div class="comment-contents">
-               <div class="comment-title">
-                <p class="comment-username">${allComments[i].username}</p>
-                <p class="comment-time">${allComments[i].timestamp}</p>
-                <a href="./voteCommentUp?id=${allComments[i].id}"><img src="./images/avatars/up.png" class="small-icon"></a><p>${allComments[i].upvote}</p>
-                <a href="./voteCommentDown?id=${allComments[i].id}"><img src="./images/avatars/down.png" class="small-icon"></a><p>${allComments[i].downvote}</p>
-                <a href="./deleteComment?id=${allComments[i].id}"><img src="./images/avatars/delete.png" class="small-icon"></a>
-               </div>
-              <p class="comment-text">${allComments[i].content}</p>
-             </div>
+                <div class="comment-avater">
+                    <img class="comment-image" src="./images/avatars/${allComments[i].avatar}.png">
+                </div>
+                <div class="comment-contents">
+                    <div class="comment-title">
+                        <p class="comment-username">${allComments[i].username}</p>
+                        <p class="comment-time">${allComments[i].timestamp}</p>
+                        <a href="./voteCommentUp?id=${allComments[i].id}"><img src="./images/avatars/up.png" class="small-icon"></a><p>${allComments[i].upvote}</p>
+                        <a href="./voteCommentDown?id=${allComments[i].id}"><img src="./images/avatars/down.png" class="small-icon"></a><p>${allComments[i].downvote}</p>
+                        <a href="./deleteComment?id=${allComments[i].id}"><img src="./images/avatars/delete.png" class="small-icon"></a>
+                    </div>
+                    <p class="comment-text">${allComments[i].content}</p>
+                </div>
             </div>`
-        }
+        };
         //reload the page but don't jump to the top...
         location.reload();
-     }
+    };
 
 });
